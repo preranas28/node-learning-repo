7e9d511985d632fc3e81c4251a2597a19a1d73a5
preranas28 <preranas2807@gmail.com>
2024-07-09 11:45:04 +0530

    NPM and Nodejs basic
    NPM and Nodejs basic - npm start and some APIs

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log(`This is a starting point....`);
+
+console.log(`----- Global Objects-----`);
+console.log(console);
+console.log(global);
+console.log(process);
+console.log(__filename);
+console.log(__dirname);
+console.log(module);
+
+
